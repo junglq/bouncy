9d405f3c9ffa2cb46a84c0411d2416805da443ad
junglq <junglqq@gmail.com>
2020-08-05 20:19:18 +0300

    next changed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,12 @@
 $(document).ready(function () {
-
     // Slider
-    $('.reviews__slider,.team__slider').slick({
+    $('.reviews__slider,.team__slider,.news__slider').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
         dots: true,
-        fade: true,
-    });
-
-    $('.news__slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        fade: true,
+        fade: true
     });
 
     // Burger 
@@ -32,17 +22,8 @@ $(document).ready(function () {
 
     $(window).on('scroll resize', function () {
         scrollOffset = $(this).scrollTop();
-        // topScroll(scrollOffset);
         checkscroll(scrollOffset);
     })
-
-    // function topScroll(scrollOffset) {
-    //     if (scrollOffset + 59 < 701) {
-    //         $('#hello').addClass('active');
-    //     } else {
-    //         $('#hello').removeClass('active');
-    //     }
-    // }
 
     function checkscroll(scrollOffset) {
         if (scrollOffset > 0) {
@@ -66,20 +47,20 @@ $(document).ready(function () {
         $('body').removeClass('lock');
 
         $("html,body").animate({
-            scrollTop: elementOffset - 59
+            scrollTop: elementOffset - 60
         }, 1000)
 
     })
 
-
+    // Arrow scroll
     let intro = $(".intro");
     let introH = intro.innerHeight();
 
-    $(".intro__arrow").on("click resize", function (event) {
+    $(".intro__arrow").on("click resize scroll", function (event) {
         event.preventDefault();
         introH = intro.innerHeight();
         $("html,body").animate({
-            scrollTop: introH - 59
+            scrollTop: introH - 60
         }, 1000)
     })
 
