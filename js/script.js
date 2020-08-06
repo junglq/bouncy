6eb0e6c144275140b347853fa777db4caa764203
junglq <junglqq@gmail.com>
2020-08-06 13:04:54 +0300

    add animation scroll

--- js/script.js
+++ js/script.js
@@ -65,17 +65,40 @@ $(document).ready(function () {
     })
 
     // Animation items
-
     const animItems = document.querySelectorAll('.anim-items');
 
     if (animItems.length > 0) {
-        function animItemHeight() {
-            for (let index = 0; index < animItems.lenght; index++) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            console.log(1);
+            for (let index = 0; index < animItems.length; index++) {
                 const animItem = animItems[index];
                 const animItemHeight = animItem.offsetHeight;
-                console.log(animItemHeight);
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('active');
+                } else {
+                    if (!animItem.classList.contains('anim-no-hide')) {
+                        animItem.classList.remove('active');
+                    }
+                }
             }
         }
-        animItemHeight();
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+        }
+        setTimeout(() => {
+            animOnScroll();
+        }, 300);
     }
 });
